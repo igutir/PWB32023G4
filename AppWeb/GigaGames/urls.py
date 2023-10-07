5fa4638c4aaf3c69f80bd02877904ec5813cff88
igutir <igutierrez@live.cl>
2023-10-07 15:07:24 -0300

    Arreglos menores

--- AppWeb/GigaGames/urls.py
+++ AppWeb/GigaGames/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     path('', home, name="home"),
     path('explorar/', explorar, name="explorar"),
-    path('categoria/<int:id>/$', categoria, name="categoria"),
+    path('categoria/<int:id>/', categoria, name="categoria"),
 
     ##Paginas para el pokemon
     path('pokedex_api/', pokedex_api, name="pokedex_api"),
@@ -28,10 +28,10 @@ urlpatterns = [
 
     path('j/<int:id>/', juego, name="juego"),
     path('mantenedor/', mantenedor_juegos, name="mantenedor_juegos"),
-    path('mantenedor/agregar_juego/$', agregar_juego, name="agregar_juego"),
+    path('mantenedor/agregar_juego/', agregar_juego, name="agregar_juego"),
     path('mantenedor/listado_juegos/', modificar_juego_lista, name="modificar_juego_lista"),
-    path('mantenedor/listado_juegos/u/<int:idjuego>/$', modificar_juego, name="modificar_juego"),
-    path('mantenedor/listado_juegos/d/<idjuego>/$', eliminar_juego, name="eliminar_juego"),
+    path('mantenedor/listado_juegos/u/<int:idjuego>/', modificar_juego, name="modificar_juego"),
+    path('mantenedor/listado_juegos/d/<idjuego>/', eliminar_juego, name="eliminar_juego"),
     
 
 
